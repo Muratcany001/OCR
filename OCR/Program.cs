084221eb240326a64b03dc7101738d3093a9e071
muratcany001 <muratcany0167@gmail.com>
2026-02-20 00:34:55 +0300

    ref(): Program.cs refactor

--- OCR/Program.cs
+++ OCR/Program.cs
@@ -5,18 +5,19 @@ namespace OCR;
 
 class Program
 {
+    private static readonly CharacterRecognition Ocr = new();
+    
     static void Main(string[] args)
     {
-        
         string filePath = "/Users/murat/RiderProjects/OCR/OCR/ExamplePhotos/DA3155805_202412181150384510.bmp";
         
         Mat processedImage = ImageProcessing.ProcessFile(filePath);
-        Stopwatch sw = Stopwatch.StartNew();
-        var ocr = new CharacterRecognition();
-        string text = ocr.Read(processedImage);
-        sw.Stop();
-        Console.WriteLine(text);
         
+        Stopwatch sw = Stopwatch.StartNew();
+        string text = Ocr.Read(processedImage);
+        sw.Stop();
+        
+        Console.WriteLine(text);
         Console.WriteLine($"Took {sw.ElapsedMilliseconds} ms");
     }
 }
--- OCR/obj/Debug/net8.0/OCR.AssemblyInfo.cs
+++ OCR/obj/Debug/net8.0/OCR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08107df1b87977bdfa19fdc2122cda50c71fe80b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc98607b8941b8c66ead1d3e9c05cd137621f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OCR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
